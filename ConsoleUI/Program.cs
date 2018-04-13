7c5643687eff37c768f0db3e38bec2b70fec5e3b
2Lomi <eggmasstree@gmail.com>
2018-04-13 22:46:17 +0200

    first commit

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -6,10 +6,35 @@ using System.Threading.Tasks;
 
 namespace ConsoleUI
 {
+    public class ConsoleUI
+    {
+        private int w { get; }
+        private int h { get; }
+        public bool cursorVisible { get; } = false;
+        public ConsoleUI()
+        {
+            w = 30;
+            h = 30;
+            cursorVisible = this.cursorVisible;
+        }
+
+        public int getW()
+        {
+            return w;
+        }
+
+        public int getH()
+        {
+            return h;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            ConsoleUI ui = new ConsoleUI();
+            Console.CursorVisible = ui.cursorVisible;
+
         }
     }
 }
